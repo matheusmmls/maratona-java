1ffb503175b5df813701d93cf71dd88f923a508b
matheusmmls <matheusmmls2006@gmail.com>
2026-04-16 02:40:01 -0300

    aulas 29, 30; break.

---
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao04.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao04 {
+    // Dado o valor de um carro, descubra em quantas ele pode ser parcelado.
+    // Condição valorParcela >= 1000.
+    public static void main(String[] args) {
+        int valorTotal = 30000;
+        for (int parcela = 1; parcela <= valorTotal; parcela++) {
+            double valorParcela = valorTotal / parcela;
+
+            if (valorParcela < 1000) {
+                break;
+            }
+            System.out.println("Parcela " + parcela + "R$ " + valorParcela);
+            //if (valorParcela >= 1000) {
+
+            //} else {
+            //break;
+            //}
+            // resolução com muita linha foi comentada ja q a o if acima ja faz tudo.
+
+        }
+
+    }
+}
